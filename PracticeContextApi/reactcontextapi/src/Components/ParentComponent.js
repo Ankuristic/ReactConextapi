9ccc503eed167ed76f1a3049a2ec35c7fcfa4712
Ankuristic <63995112+Ankuristic@users.noreply.github.com>
2023-12-30 12:45:10 +0530

    react_tailwind

--- PracticeContextApi/reactcontextapi/src/Components/ParentComponent.js
+++ PracticeContextApi/reactcontextapi/src/Components/ParentComponent.js
@@ -1,16 +1,21 @@
-import {useState}  from 'react'
+import {useState}  from 'react';
+import ChildComponent from './ChildComponent';
+import { colorContext } from '../context';
 
 const ParentComponent = (props) => {
+  console.log(colorContext);
     const [color,setColor]=useState["#000000"]
   return (
    <>
+   <colorContext.Provider>
 <h1> Pick a color </h1>
 <input type ="color" onChange={(e)=>{
     setColor(e.target.value)
 }}
 value={color} />
-<childComponent color ={color}/>
-
+{/* <colorContext.Provider value ="red"> */}
+<ChildComponent color ={color}/>
+</colorContext.Provider>
    </>
   )
 }
---
+++ PracticeContextApi/reactcontextapi/src/context.js
@@ -0,0 +1,6 @@
+import { createContext } from "react";
+
+export const colorContext = createContext();
+
+//1. creating context
+//
